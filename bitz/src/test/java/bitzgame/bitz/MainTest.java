8e9a4ac02710c8916f6723b7aa7647f7f94618d6
himotimo <timo.ok.ijas@gmail.com>
2016-09-07 01:46:18 +0300

    lisasin bomiin dependecyt slickia varten, assembly (mavenin plugin joka vaaditaan buildaukseen), java-virtuaalikoneen natiivikirjastojen pathit, pit, dokumentit

---
+++ bitz/src/test/java/bitzgame/bitz/MainTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bitzgame.bitz;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Timo
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+
+    /**
+     * Test of sum method, of class Main.
+     */
+    
+}
