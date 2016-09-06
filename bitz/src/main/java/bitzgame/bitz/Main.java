cf7f29f13a874793acf169cd98c91b7b2a659fb5
himotimo <timo.ok.ijas@gmail.com>
2016-09-07 02:16:55 +0300

    lisasin kuvan ja liikkeen sille

--- bitz/src/main/java/bitzgame/bitz/Main.java
+++ bitz/src/main/java/bitzgame/bitz/Main.java
@@ -6,25 +6,34 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Main extends BasicGame
 {
+    Image newimg;
+    int y_pos;
 	public Main(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+            newimg = new Image("src/assets/bg_pattern.png");
+            y_pos = 0;
+        }
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+            y_pos += 1;
+        }
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
 		g.drawString("Howdy!", 10, 10);
+                newimg.draw(20, 20+y_pos);
 	}
 
 	public static void main(String[] args)
