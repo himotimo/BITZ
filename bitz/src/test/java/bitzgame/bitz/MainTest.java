405d59554461ef528959da526b4e82e6227bbe10
Pyry Kontio <pyry.kontio@drasa.eu>
2016-09-17 15:31:32 +0300

    Testing works now! Implemented a GameTestCtx that runs the test send to it by a BlockingQueue.

--- bitz/src/test/java/bitzgame/bitz/MainTest.java
+++ bitz/src/test/java/bitzgame/bitz/MainTest.java
@@ -5,75 +5,29 @@
  */
 package bitzgame.bitz;
 
-import java.util.function.Function;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.function.Supplier;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
 
 /**
  *
  * @author Timo
  */
-class GameTestCtx extends BasicGame implements Runnable {
-
-    public GameTestCtx(String title) {
-        super(title);
-    }
-
-    @Override
-    public void init(GameContainer gc) throws SlickException {
-    }
-    
-    Function<Integer, Integer> nextTest;
-
-    @Override
-    public void update(GameContainer gc, int i) throws SlickException {
-        if (MainTest.doIt) {
-            Integer result = GameTestCtx.nextTest(new Integer(0));
-            MainTest.doIt = false;
-        }
-    }
-
-    @Override
-    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
-    }
-
-    @Override
-    public void run() {
-        try {
-            GameTestCtx game = new GameTestCtx("Simple Slick Game");
-            MainTest.container = new AppGameContainer(game);
-            MainTest.container.setDisplayMode(640, 480, false);
-            MainTest.container.start();
-
-        } catch (SlickException ex) {
-        }
-    }
-
-}
 
 public class MainTest {
 
     public MainTest() {
     }
 
-    static AppGameContainer container;
-
     @BeforeClass
     public static void setUpClass() {
-        doIt = false;
-        (new Thread(new GameTestCtx("TestCtx"))).start();
+        GameTestCtx.setup();
     }
 
     @AfterClass
@@ -91,22 +45,22 @@ public class MainTest {
     /**
      * Test of main method, of class Main.
      */
-    static boolean doIt;
-    public static Function<Integer, Integer> nextTest;
-
     @Test
-    public void imageTest() {
-        nextTest = (input) -> {
+    public void imageTest() throws InterruptedException {
+        // NOTE to Timo: Do NOT use assertions inside the () -> { test }! Only catch errors and return them.
+        GameTestCtx.inputQueue.put(() -> {
             try {
                 Image test = new Image("src/assets/spr_item_crest.png");
-                assertNotEquals("joo", test, null);
+                if (test != null) {
+                    throw new Exception();
+                }
+                return GameTestCtx.ok;
             } catch (Exception e) {
-                System.out.println("caught");
+                return e;
             }
-            return 1;
-        };
-        doIt = true;
+        });
+        Throwable ex = GameTestCtx.returnQueue.take();
+        assertEquals("Did it work?", ex, GameTestCtx.ok);
     }
-
 
 }
